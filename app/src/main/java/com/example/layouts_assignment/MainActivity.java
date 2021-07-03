fe2427d2e58a38a53f2ce904bfcc8f740f2732a0
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-07-04 00:50:19 +0530

    playstore layout added

--- app/src/main/java/com/example/layouts_assignment/MainActivity.java
+++ app/src/main/java/com/example/layouts_assignment/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ticket_booking_layout);
+        setContentView(R.layout.playstore_page);
     }
 }
