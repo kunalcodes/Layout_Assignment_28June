09f95399c081f8f849577b9d638b0b275106432b
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-07-04 01:40:40 +0530

    insurance layout added

--- app/src/main/java/com/example/layouts_assignment/MainActivity.java
+++ app/src/main/java/com/example/layouts_assignment/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.playstore_page);
+        setContentView(R.layout.insurance_layout);
     }
 }
