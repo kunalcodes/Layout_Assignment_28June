36566a85a56d1e6c3c6299f63c0b6aa0961b7e6e
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-07-04 00:16:47 +0530

    ticket layout added

--- app/src/main/java/com/example/layouts_assignment/MainActivity.java
+++ app/src/main/java/com/example/layouts_assignment/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.masai_login);
+        setContentView(R.layout.ticket_booking_layout);
     }
 }
